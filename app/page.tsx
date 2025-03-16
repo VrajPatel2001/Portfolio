a94287623c43e694d830a8e131f1378f9c395cf2
Vraj Patel <vrajnp2001@gmail.com>
2025-03-16 00:12:42 -0600

    style: change header position from fixed to sticky

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Portfolio() {
       
       <main className="relative z-10">
         {/* Navigation */}
-        <header className="fixed top-0 left-0 right-0 z-50">
+        <header className="sticky top-0 z-50">
           <div className="container mx-auto px-4 py-4">
             <nav suppressHydrationWarning className="flex justify-end items-center relative">
               <button
